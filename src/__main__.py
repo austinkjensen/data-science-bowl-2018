59e338f843e9d0bf554fdd8d228001c9b2d4afe1
Luke Weber <lukedottec@gmail.com>
2018-01-29 22:11:39 -0800

    So AJ can view images big time wowaweewo

--- src/__main__.py
+++ src/__main__.py
@@ -28,20 +28,22 @@ def scrub_pics():
 	'''Let's check out these dank pics.
 	'''
 	# Grab locations of PNG training data (not raw data)
-	train_in = []
-	train_out = []
+	train_xs = []
+	train_ys = []
 	train_data_dir = DATA_DIR + TRAIN_DIR
 	for el in os.listdir(train_data_dir):
 		el_rel = os.path.join(train_data_dir, el)
 		images_rel = os.path.join(el_rel, IMAGES_SUB_DIR)
 		masks_rel = os.path.join(el_rel, MASKS_SUB_DIR)
-		train_in.append([os.path.join(images_rel, x) for x in os.listdir(images_rel)])
-		train_out.append([os.path.join(masks_rel, x) for x in os.listdir(masks_rel)])
+		train_xs.append([os.path.join(images_rel, x) for x in os.listdir(images_rel)])
+		train_ys.append([os.path.join(masks_rel, x) for x in os.listdir(masks_rel)])
 
-	print(train_in)
-	image_id = '7ba20aa731cc21af74a8d940254176cbad1bdc44f240b550341c6d9c27509daa'
-	image = Image.open(DATA_DIR + TRAIN_DIR + image_id + '/images/' + image_id + '.png')
-	image.show()
+	# View the (x, y) pairs
+	for i in range(max(len(train_xs), len(train_ys))):
+		image_loc = train_xs[i]
+		image = Image.open(image_loc)
+		image.show()
+		#print(train_ys[i])
 
 if __name__ == '__main__':
 	print('Literally, Hello, World')
